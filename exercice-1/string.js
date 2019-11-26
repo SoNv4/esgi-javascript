3bfcecf71ef24eb5ed6aa4785e3bd2912a9d89eb
Sofiane Miannay <sofianemiannay@gmail.com>
2019-11-26 13:04:43 +0100

    ADD string.js

---
+++ exercice-1/string.js
@@ -0,0 +1,56 @@
+function ucFirst(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+    return value[0].toUpperCase() + value.substring(1);
+}
+
+function capitalize(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+    var string = value.toLowerCase().split(' ').map(elem => ucFirst(elem)).join(' ');
+    return string;
+}
+
+function camelCase(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+    var string = value.toLowerCase().split(' ').map(elem => ucFirst(elem)).join('');
+    return string;
+}
+
+function snake_case(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+    var string = value.split(' ').join('_');
+    return string;
+}
+
+function leet(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+}
+
+function prop_access(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+}
+
+function verlan(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+    var reverse = value.split("").reverse().join('');
+    return reverse;
+}
+
+function yoda(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+    var reverse  = value.split(' ').reverse().join(' ');
+    return reverse;
+}
+
+function vig(value) {
+    if(typeof value !== "string" || value.length === 0) return undefined;
+}
+
+console.log(ucFirst('hello world'));
+console.log(capitalize('hello world'));
+console.log(camelCase('hello world'));
+console.log(snake_case('hello world'));
+console.log(leet('anaconda'));
+console.log(prop_access('test test'));
+console.log(verlan('Hello world'));
+console.log(yoda('Hello world'));
+console.log(vig(''));
